5b0b85e9f6d43e8b94fe834ad08009b6843821d1
alessiam1909 <ale.menon.vr@gmail.com>
2023-01-11 17:49:53 +0100

    sistemazione file e creazione file scss

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,68 @@
+<script>
+export default {
+    data() {
+        return {
+            menu: [
+                {
+                    label: 'CHARACTERS',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'COMICS',
+                    url: '#',
+                    active: true
+                },
+                {
+                    label: 'MOVIES',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'TV',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'GAMES',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'COLLECTIBLES',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'VIDEOS',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'FANS',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'NEWS',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'SHOP',
+                    url: '#',
+                    active: false
+                }
+            ]
+        }
+    }
+}
+</script>
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<style lang="sass">
+    
+</style>
